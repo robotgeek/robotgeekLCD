55666503d9cce72b2bd7df875f43973ad5742bad
Kyle Granat <kyle@trossenrobotics.com>
2013-11-13 22:00:57 -0600

    Examples now work + documentation + moved all data to subfolder

--- robotgeekLCD/RobotGeekLCD.cpp
+++ robotgeekLCD/RobotGeekLCD.cpp
@@ -1,58 +1,86 @@
-#include "robotgeekLCD.h"
+ /***********************************************************************************
+ *       ___________________________________
+ *      |  _______________________________  |
+ *      | |RobotGeek I2C LCD              |\|
+ *      | |  RobotGeekLCD.cpp             | |
+ *      | |                               | |
+ *      | |_______________________________|/|
+ *      |___________________________________|
+ *
+ *  This library will allow use of the RobotGeek IIC LCD
+ *   http://learn.trossenrobotics.com/30-robotgeek-getting-started-guides/dev-kits/57-robotgeek-i2c-lcd-getting-started-guide
+ *   http://www.trossenrobotics.com/robotgeek-lcd
+ *     
+ *	The RobotGeek IIC LCD has 3 components
+ *	1)16x2 LCD
+ *	2)Hitachi HD44780 LCD Driver
+ *	3)pcf8574 I2C - I2C IO Expander
+ *	
+ *	The HD44780 is controlled by 3 control pins and 8 data pins. These pins
+ *	are controlled by the pcf8574 I2C chip. By sending IIC data to this chip
+ *	the controller can control the HD44780 and the LCD. The pcf8574 also gives
+ *	access to the backlight.
+ *
+ *
+ *
+ *  Wiring
+ *    RobotGeek Sensor Shield With Geekduino/Arduino Uno/Seeeduino
+ *      The RobotGeek I2C LCD will plug directly into the 4-pin I2C port on
+ *      the sensor shield using a 4-pin sensor cable. Normally the sensor cable
+ *      will be plugged in with the following orientation:
+ *        SCL    - Yellow
+ *        SDA    - White
+ *        5v/VCC - Red
+ *        G/GND  - Black
+ *  
+ *    Geekduino/Arduino/Seeeduino 
+ *       The RobotGeek I2C LCD can be plugged direcly into the pins of a 
+ *        ~duino board using Female-Female jumper cables and pin couplers
+ *        LCD - ~duino Pin 
+ *        SCL - A5
+ *        SDA - A4
+ *        VCC - 5v
+ *        GND - Gnd
+ *        
+ *    ArbotiX
+ *        LCD - ~duino Pin 
+ *        SCL - D16 9Pin closest to processor0
+ *        SDA - D17 9Pin closest to processor0
+ *        VCC - 5v Pin on D17 (Middle Pin)
+ *        GND - Gnd Pin on D17(Pin closest to the edge of the board)
+ *
+ *
+ *  References
+ *    This example and the RobotGeekLCD library is based off the Arduino
+ *    LiquidCrystal Library and examples
+ *    http://www.arduino.cc/en/Tutorial/LiquidCrystal
+ *     This example code is in the public domain.
+ *
+ *
+ ***********************************************************************************/
+ 
 
-
+#include "RobotGeekLCD.h"
 #include <inttypes.h>
 #include "Arduino.h"
-
 #include <Wire.h>
-
-
-// When the display powers up, it is configured as follows:
-//
-// 1. Display clear
-// 2. Function set: 
-//    DL = 1; 8-bit interface data 
-//    N = 0; 1-line display 
-//    F = 0; 5x8 dot character font 
-// 3. Display on/off control: 
-//    D = 0; Display off 
-//    C = 0; Cursor off 
-//    B = 0; Blinking off 
-// 4. Entry mode set: 
-//    I/D = 1; Increment by 1 
-//    S = 0; No shift 
-//
-// Note, however, that resetting the Arduino doesn't reset the LCD, so we
-// can't assume that its in that state when a sketch starts (and the
-// robotgeekLCD constructor is called).
-
-//Rs-p0
-//R/W-p2
-//E-p2
-//Backlight-p3
-//DB4-p4
-//DB5-p5
-//DB6-p6
-//DB7-p7
-
 
 //the LCD will always be IIC, so we'll strip out alot of the LiquidCrystal Library
 
 
-
-robotgeekLCD::robotgeekLCD(uint8_t address)
+RobotGeekLCD::RobotGeekLCD()
 {
- // init(0x27);//don't call init from here! all sorts of weirdness from the wire library
-  _backlight = 1;
+	//init(0x27);//don't call init from here! all sorts of weirdness from the wire library
+  	_backlight = 1; //backlight will be set on by default
 }
 
-
-
-//always four bit mode
-void robotgeekLCD::init(uint8_t address)
+//always four bit mode for the rg lcd
+void RobotGeekLCD::init()
 {
-
-  Wire.begin(); // join i2c bus (address optional for master)
+  //I2C address for the RobotGeek I2C LCD - this is hard wired to 0x27
+  int address =0x27;	
+  
+  Wire.begin(); // join i2c bus
 	
   _rs_pin =0;
   _rw_pin = 1;
@@ -67,21 +95,17 @@ void robotgeekLCD::init(uint8_t address)
   _data_pins[6] = 6;
   _data_pins[7] = 7; 
 
-//always 4 bit mode
-    _displayfunction = LCD_4BITMODE | LCD_2LINE | LCD_5x8DOTS;
+  //always 4 bit mode
+  _displayfunction = LCD_4BITMODE | LCD_2LINE | LCD_5x8DOTS;
 
-
-
-	//always 16/x
+  //start lcd
   begin(address);  
 }
 
-void robotgeekLCD::begin(uint8_t address) {
-
-
-  _numlines = 2;
-  _currline = 0;
-
+void RobotGeekLCD::begin(uint8_t address) 
+{
+	_numlines = 2;
+	_currline = 0;
 
 	int tadd = address;
 
@@ -90,16 +114,14 @@ void robotgeekLCD::begin(uint8_t address) {
   // before sending commands. Arduino can turn on way befer 4.5V so we'll wait 50
   delayMicroseconds(50000); 
   // Now we pull both RS and R/W low to begin commands
-  
   _iicLastSent = B00000000;
-  
-	writeIIC(0);
+  writeIIC(0);
   /*
 	Wire.beginTransmission(0x27); // transmit to device 0x27, lcd
 	Wire.write(_iicLastSent);              // sends one byte, sets RS,R/W, and enable low  
-delay(1000);
+	delay(1000);
 	Wire.endTransmission();   
-*/
+  */
 
 
     // this is according to the hitachi HD44780 datasheet
@@ -120,45 +142,40 @@ delay(1000);
     // finally, set to 4-bit interface
     write4bits(0x02); 
 
+	// finally, set # lines, font size, etc.
+	command(LCD_FUNCTIONSET | _displayfunction);  
 
+	// turn the display on with no cursor or blinking default
+  	_displaycontrol = LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF;  
+  	noDisplay();
 
-  // finally, set # lines, font size, etc.
-  command(LCD_FUNCTIONSET | _displayfunction);  
+  	// clear it off
 
+  	clear();
+   
 
-  // turn the display on with no cursor or blinking default
-  _displaycontrol = LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKOFF;  
-  noDisplay();
-
-  // clear it off
-
-  clear();
-  
-  
-
-  // Initialize to default text direction (for romance languages)
-  _displaymode = LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT;
-  // set the entry mode
-  command(LCD_ENTRYMODESET | _displaymode);
-  
+  	// Initialize to default text direction (for romance languages)
+  	_displaymode = LCD_ENTRYLEFT | LCD_ENTRYSHIFTDECREMENT;
+  	// set the entry mode
+  	command(LCD_ENTRYMODESET | _displaymode);
+    
     display();
-
 }
 
 /********** high level commands, for the user! */
-void robotgeekLCD::clear()
+void RobotGeekLCD::clear()
 {
   command(LCD_CLEARDISPLAY);  // clear display, set cursor position to zero
   delayMicroseconds(2000);  // this command takes a long time!
 }
 
-void robotgeekLCD::home()
+void RobotGeekLCD::home()
 {
   command(LCD_RETURNHOME);  // set cursor position to zero
   delayMicroseconds(2000);  // this command takes a long time!
 }
 
-void robotgeekLCD::setCursor(uint8_t col, uint8_t row)
+void RobotGeekLCD::setCursor(uint8_t col, uint8_t row)
 {
   int row_offsets[] = { 0x00, 0x40, 0x14, 0x54 };
   if ( row >= _numlines ) {
@@ -169,98 +186,27 @@ void robotgeekLCD::setCursor(uint8_t col, uint8_t row)
 }
 
 // Turn the display on/off (quickly)
-void robotgeekLCD::noDisplay() {
+void RobotGeekLCD::noDisplay() {
   _displaycontrol &= ~LCD_DISPLAYON;
   command(LCD_DISPLAYCONTROL | _displaycontrol);
 }
-void robotgeekLCD::display() {
+void RobotGeekLCD::display() {
   _displaycontrol |= LCD_DISPLAYON;
   command(LCD_DISPLAYCONTROL | _displaycontrol);
 }
 
 
 // Turn the display on/off (quickly)
-void robotgeekLCD::nooBacklight() {
-
-
+void RobotGeekLCD::noBacklight() {
 
 		_iicLastSent = _iicLastSent & (~B00001000);
 		Wire.beginTransmission(0x27); // transmit to device 0x27, lcd
 		Wire.write(_iicLastSent);              // sends one byte, sets RS,R/W, and enable low  
 		Wire.endTransmission();    // stop transmitting
-		
-		delay(1000);
-		
-		
-		_iicLastSent = _iicLastSent | B00001000;
-		
-		Wire.beginTransmission(0x27); // transmit to device 0x27, lcd
-		Wire.write(_iicLastSent);              // sends one byte, sets RS,R/W, and enable low  
-		Wire.endTransmission();    // stop transmitting
-		
-		
-		delay(1000);
-		
-			
-		_iicLastSent = _iicLastSent & (~B00001000);
-		Wire.beginTransmission(0x27); // transmit to device 0x27, lcd
-		Wire.write(_iicLastSent);              // sends one byte, sets RS,R/W, and enable low  
-		Wire.endTransmission();    // stop transmitting
-		
-		delay(1000);
-		
-		
-		_iicLastSent = _iicLastSent | B00001000;
-		
-		Wire.beginTransmission(0x27); // transmit to device 0x27, lcd
-		Wire.write(_iicLastSent);              // sends one byte, sets RS,R/W, and enable low  
-		Wire.endTransmission();    // stop transmitting
-		
-		
-		delay(1000);
-		
-			
-		_iicLastSent = _iicLastSent & (~B00001000);
-		Wire.beginTransmission(0x27); // transmit to device 0x27, lcd
-		Wire.write(_iicLastSent);              // sends one byte, sets RS,R/W, and enable low  
-		Wire.endTransmission();    // stop transmitting
-		
-		delay(1000);
-		
-		
-		_iicLastSent = _iicLastSent | B00001000;
-		
-		Wire.beginTransmission(0x27); // transmit to device 0x27, lcd
-		Wire.write(_iicLastSent);              // sends one byte, sets RS,R/W, and enable low  
-		Wire.endTransmission();    // stop transmitting
-		
-		
-		delay(1000);
-		
-				
-			
-		
 		
 }
 
-// Turn the display on/off (quickly)
-void robotgeekLCD::noBacklight() {
-
-
-
-		_iicLastSent = _iicLastSent & (~B00001000);
-		Wire.beginTransmission(0x27); // transmit to device 0x27, lcd
-		Wire.write(_iicLastSent);              // sends one byte, sets RS,R/W, and enable low  
-		Wire.endTransmission();    // stop transmitting
-
-
-				
-			
-		
-		
-}
-
-void robotgeekLCD::backlight() {
+void RobotGeekLCD::backlight() {
 		_iicLastSent = _iicLastSent | B00001000;
 		
 		Wire.beginTransmission(0x27); // transmit to device 0x27, lcd
@@ -273,60 +219,60 @@ void robotgeekLCD::backlight() {
 
 
 // Turns the underline cursor on/off
-void robotgeekLCD::noCursor() {
+void RobotGeekLCD::noCursor() {
   _displaycontrol &= ~LCD_CURSORON;
   command(LCD_DISPLAYCONTROL | _displaycontrol);
 }
-void robotgeekLCD::cursor() {
+void RobotGeekLCD::cursor() {
   _displaycontrol |= LCD_CURSORON;
   command(LCD_DISPLAYCONTROL | _displaycontrol);
 }
 
 // Turn on and off the blinking cursor
-void robotgeekLCD::noBlink() {
+void RobotGeekLCD::noBlink() {
   _displaycontrol &= ~LCD_BLINKON;
   command(LCD_DISPLAYCONTROL | _displaycontrol);
 }
-void robotgeekLCD::blink() {
+void RobotGeekLCD::blink() {
   _displaycontrol |= LCD_BLINKON;
   command(LCD_DISPLAYCONTROL | _displaycontrol);
 }
 
 // These commands scroll the display without changing the RAM
-void robotgeekLCD::scrollDisplayLeft(void) {
+void RobotGeekLCD::scrollDisplayLeft(void) {
   command(LCD_CURSORSHIFT | LCD_DISPLAYMOVE | LCD_MOVELEFT);
 }
-void robotgeekLCD::scrollDisplayRight(void) {
+void RobotGeekLCD::scrollDisplayRight(void) {
   command(LCD_CURSORSHIFT | LCD_DISPLAYMOVE | LCD_MOVERIGHT);
 }
 
 // This is for text that flows Left to Right
-void robotgeekLCD::leftToRight(void) {
+void RobotGeekLCD::leftToRight(void) {
   _displaymode |= LCD_ENTRYLEFT;
   command(LCD_ENTRYMODESET | _displaymode);
 }
 
 // This is for text that flows Right to Left
-void robotgeekLCD::rightToLeft(void) {
+void RobotGeekLCD::rightToLeft(void) {
   _displaymode &= ~LCD_ENTRYLEFT;
   command(LCD_ENTRYMODESET | _displaymode);
 }
 
 // This will 'right justify' text from the cursor
-void robotgeekLCD::autoscroll(void) {
+void RobotGeekLCD::autoscroll(void) {
   _displaymode |= LCD_ENTRYSHIFTINCREMENT;
   command(LCD_ENTRYMODESET | _displaymode);
 }
 
 // This will 'left justify' text from the cursor
-void robotgeekLCD::noAutoscroll(void) {
+void RobotGeekLCD::noAutoscroll(void) {
   _displaymode &= ~LCD_ENTRYSHIFTINCREMENT;
   command(LCD_ENTRYMODESET | _displaymode);
 }
 
 // Allows us to fill the first 8 CGRAM locations
 // with custom characters
-void robotgeekLCD::createChar(uint8_t location, uint8_t charmap[]) {
+void RobotGeekLCD::createChar(uint8_t location, uint8_t charmap[]) {
   location &= 0x7; // we only have 8 locations 0-7
   command(LCD_SETCGRAMADDR | (location << 3));
   for (int i=0; i<8; i++) {
@@ -336,11 +282,11 @@ void robotgeekLCD::createChar(uint8_t location, uint8_t charmap[]) {
 
 /*********** mid level commands, for sending data/cmds */
 
-inline void robotgeekLCD::command(uint8_t value) {
+inline void RobotGeekLCD::command(uint8_t value) {
   send(value, LOW);
 }
 
-inline size_t robotgeekLCD::write(uint8_t value) {
+inline size_t RobotGeekLCD::write(uint8_t value) {
   send(value, HIGH);
   return 1; // assume sucess
 }
@@ -350,7 +296,7 @@ inline size_t robotgeekLCD::write(uint8_t value) {
 /************ low level data pushing commands **********/
 
 // write either command or data, with automatic 4/8-bit selection
-void robotgeekLCD::send(uint8_t value, uint8_t mode) {
+void RobotGeekLCD::send(uint8_t value, uint8_t mode) {
 
 	//int tempValue = mode;//rspin high
 	//tempValue = tempValue &(~(1<<2))//rw_pin low
@@ -387,7 +333,7 @@ void robotgeekLCD::send(uint8_t value, uint8_t mode) {
   //}
 }
 
-void robotgeekLCD::pulseEnable(uint8_t current) {
+void RobotGeekLCD::pulseEnable(uint8_t current) {
 
 
    
@@ -416,27 +362,18 @@ void robotgeekLCD::pulseEnable(uint8_t current) {
 
 }
 
-void robotgeekLCD::write4bitsCommand(uint8_t value){
+void RobotGeekLCD::write4bitsCommand(uint8_t value){
 
 	int tempValue = (value<<4) | (_backlight<<3);//shift the value up 4 bits, add backlight bit. En,RW,RS are all 0
-
-	
-	
-	
-	
 	Wire.beginTransmission(0x27); // transmit to device 0x27, lcd
 	Wire.write(tempValue);              // sends one byte, sets RS,R/W, and enable low  
 	Wire.endTransmission();    // stop transmitting
 	pulseEnable(tempValue);
 }
 
-void robotgeekLCD::write4bitsData(uint8_t value){
+void RobotGeekLCD::write4bitsData(uint8_t value){
 
 	int tempValue = (value << 4) | (_backlight <<3)| 0x1;///shift the value up 4 bits, add backlight bit. En,RW, are 0, rs = 1
-
-	
-	
-	
 	Wire.beginTransmission(0x27); // transmit to device 0x27, lcd
 	Wire.write(tempValue);              // sends one byte, sets RS,R/W, and enable low  
 	Wire.endTransmission();    // stop transmitting
@@ -444,7 +381,7 @@ void robotgeekLCD::write4bitsData(uint8_t value){
 }
 
 
-void robotgeekLCD::write4bits(uint8_t value)
+void RobotGeekLCD::write4bits(uint8_t value)
 {
 
 	int tempValue = value << 4;//shift the value up 8 bits
@@ -459,7 +396,7 @@ void robotgeekLCD::write4bits(uint8_t value)
 }
 
 
-void robotgeekLCD::writeIIC(uint8_t _data){                                        
+void RobotGeekLCD::writeIIC(uint8_t _data){                                        
 	Wire.beginTransmission(0x27);
 	Wire.write(0);
 	Wire.endTransmission();   
